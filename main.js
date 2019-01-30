8f1bb3dec3cbb8275681dd190f1c021692eb7f93
scmp-craft <scmp.craft@gmail.com>
2019-01-30 14:50:20 +0800

    limit ping duration

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ var http = require("http");
 var moment = require("moment");
 
 function ping () {
-  http.get(process.env.URL, function(res) {
-    res.on('data', function(chunk) {
-      console.log("[" + res.statusCode + "]" + "[" + moment().format('LLL') + "]" + chunk);
+  var currentHour = parseInt(moment().utcOffset(8).format('HH'))
+  console.log('currentHour', currentHour);
+  if (currentHour >= 8 || currentHour < 2 ) {
+    http.get(process.env.URL, function(res) {
+      res.on('data', function(chunk) {
+        console.log("[" + res.statusCode + "]" + chunk);
+      });
     });
-  });
+  }
 }
 
 setInterval(function() {
