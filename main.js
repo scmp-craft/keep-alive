7926dc3b0d8776b5ece79124e2cbab7865f47022
scmp-craft <scmp.craft@gmail.com>
2019-01-30 13:50:28 +0800

    add log to server

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 var http = require("http");
 
-setInterval(function() {
+function ping () {
   var html = http.get(process.env.URL);
   console.log(html);
+}
+
+setInterval(function() {
+  ping()
 }, 5 * 60 * 1000);
+
+console.log('Keep alive - Hello world');
+ping()
